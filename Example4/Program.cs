e559292d5d036830ac943d8994ccfd8be90e5cc5
Evgeniy <estrin.j@yandex.ru>
2022-11-11 15:49:26 +0300

    Random

---
+++ Example4/Program.cs
@@ -0,0 +1,5 @@
+﻿double numbera = new Random().Next(1, 10);//1... 9;
+Console.WriteLine(numbera);
+double numberb = new Random().Next(1,  10);
+Console.WriteLine(numberb);
+Console.WriteLine(numberb / numbera);
